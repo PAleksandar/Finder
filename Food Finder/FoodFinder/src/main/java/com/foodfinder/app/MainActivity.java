b1cbaba70ced1a650347314d5503963ce37b38f9
PAleksandar <aleksandar.petkovic@elfak.rs>
2019-06-15 22:48:53 +0200

    firebase

--- Food Finder/FoodFinder/src/main/java/com/foodfinder/app/MainActivity.java
+++ Food Finder/FoodFinder/src/main/java/com/foodfinder/app/MainActivity.java
@@ -2,6 +2,9 @@ package com.foodfinder.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.foodfinder.authentication.TestBase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //FireBaseTest bt=new FireBaseTest();
+        TestBase bt=new TestBase();
+
+        Log.d("!!!!!!!!radi!!!!", "onCreate:  ");
+        bt.readData();
+        Log.d("!!!!!!!!radi!!!!", "onCreate:  ");
+
     }
 }
---
+++ Food Finder/authentication/src/main/java/com/foodfinder/authentication/TestBase.java
@@ -0,0 +1,31 @@
+package com.foodfinder.authentication;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class TestBase {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("test");
+
+    public void readData()
+    {
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("radi!!!!!!!!!!!!!", "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.w("error", "Failed to read value.", databaseError.toException());
+            }
+        });
+    }
+}
